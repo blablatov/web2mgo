ff3442cc860e6e9f82ccae9eef48ef7a59d5bf34
Андрей <96768789+blablatov@users.noreply.github.com>
2022-05-22 18:17:38 +0500

    Update main.go

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	tp.Name = r.URL.Path[:i]   // get slice of string before symbol ":"
 	tp.Data = r.URL.Path[i+1:] // get slice of string after symbol ":"
 	fmt.Println("Name:", strings.TrimPrefix(tp.Name, "/"), "\nData:", tp.Data)
-
-	go web2mgo.ODatareq(strings.TrimPrefix(tp.Name, "/"), tp.Data)
+	
+	done := make(chan bool)   // канал синхронизации, channel of synchronization
+	go web2mgo.ODatareq(strings.TrimPrefix(tp.Name, "/"), tp.Data, done)
 }
